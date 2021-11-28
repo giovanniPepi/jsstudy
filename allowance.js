8fa30a68485616bf6553ae86679c3bf8f018de16
gerijeb <gtpepi@hotmail.com>
2021-11-27 23:08:57 -0300

    Add weather exercise.

---
+++ allowance.js
@@ -0,0 +1,23 @@
+const checkBox = document.querySelector('input');
+const para = document.querySelector('p');
+let taskDone = false;
+
+/*Listen for user input in checkbox and calls function */
+checkBox.addEventListener('change', function() {
+    checkBox.disabled = false;
+    taskDone = true;
+    updateAllowance();
+});
+
+/*Update allowance value based on checkbox change */
+function updateAllowance() {
+    if (taskDone === true) {
+        var childAllowance = 13.99; 
+    } else {
+        var childAllowance = 5.99;
+    }
+    para.textContent = 'Child has earned R$' + childAllowance + 'this week.'
+}
+
+updateAllowance();
+
