766f332344bb2dae33dd1286074bd7fb4d854add
gerijeb <gtpepi@hotmail.com>
2021-12-04 23:56:36 -0300

    Add a gentle warning for arrow.js

--- scope/arrow.js
+++ scope/arrow.js
@@ -1,4 +1,6 @@
 const textBox = document.querySelector("#textBox");
 const output = document.querySelector("#output");
 
-textBox.addEventListener('keydown', event => output.textContent = `You pressed "${event.key}".`);
+// Ternary operator to add if/else to this function //
+textBox.addEventListener('keydown', event => (event.key === 'z' | event.key === 'Z')? output.textContent = alert(`OH MY GOD! ARE YOU SERIOUSLY TRYING TO PRESS THE FUCKING LETTER Z???????? WHAT THE HELL ARE YOU EVEN THINKING? DO NOT FUCKING PRESS Z! THIS COULD LEAD TO TOTAL SYSTEM FAILURE!`) : output.textContent = `You pressed "${event.key}".`);
+
