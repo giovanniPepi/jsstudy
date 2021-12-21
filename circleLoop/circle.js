d82b92df3501b9fef9a2ba94ac67f7b11f16ce48
gerijeb <gtpepi@hotmail.com>
2021-12-21 12:37:04 -0300

    Add different circles on circles.js

--- circleLoop/circle.js
+++ circleLoop/circle.js
@@ -1,25 +1,53 @@
-const btn = document.querySelector('button');
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
+const btn1 = document.querySelector('button');
+const btn2 = document.querySelectorAll('button')[1];
+const btn3 = document.querySelectorAll('button')[2];
+const canv = document.querySelector('canvas');
+const ctx = canv.getContext('2d');
 
-const WIDTH = document.documentElement.clientWidth;
-const HEIGHT = document.documentElement.clientHeight;
+const WDT = document.documentElement.clientWidth;
+const HGT = document.documentElement.clientHeight;
 
-canvas.width = WIDTH;
-canvas.height = HEIGHT;
+canv.width = WDT;
+canv.height = HGT;
 
-function random (number) {
-    return Math.floor(Math.random()*number);
-}
+rnd = (numb) => Math.floor(Math.random()*numb);
 
-function draw () {
-    ctx.clearRect(0,0,WIDTH, HEIGHT);
+drawBig = () => {
+    ctx.clearRect(0,0,WDT, HGT);
     for (let i = 0; i < 100; i++) {
     ctx.beginPath();
-    ctx.fillStyle = 'rgba(255,0,0,0.5)';
-    ctx.arc(random(WIDTH), random(HEIGHT), random(50), 0, 2 * Math.PI);
+    ctx.fillStyle = 'rgba(0,150,0,0.5)';
+    ctx.arc(rnd(WDT), rnd(HGT), rnd(100), 0, 2 * Math.PI);
     ctx.fill();
     }
 }
 
-btn.addEventListener('click', draw);
+drawSmall = () => {
+    ctx.clearRect(0,0,WDT, HGT);
+    for (let i = 0; i < 10000; i++) {
+    ctx.beginPath();
+    ctx.fillStyle = 'rgba(0,150,0,0.5)';
+    ctx.arc(rnd(WDT), rnd(HGT), rnd(10), 0, 2 * Math.PI);
+    ctx.fill();
+    }
+}
+
+drawInfect = () => {
+    ctx.clearRect(0,0,WDT, HGT);
+    for (let i = 0; i < 10000; i++) {
+    ctx.beginPath();
+    ctx.fillStyle = 'rgba(0,150,0,0.5)';
+    ctx.arc(rnd(WDT), rnd(HGT), rnd(10), 0, 2 * Math.PI);
+    ctx.fill();
+    }
+    for (let i = 0; i < 1000; i++) {
+    ctx.beginPath();
+    ctx.fillStyle = 'rgba(255,0,0,0.5)';
+    ctx.arc(rnd(WDT), rnd(HGT), rnd(10), 0, 2 * Math.PI);
+    ctx.fill();
+    }
+}
+
+btn1.addEventListener('click', drawBig);
+btn2.addEventListener('click', drawSmall);
+btn3.addEventListener('click', drawInfect);
