203d673213edf39104a0b26c1ed6801cfddf661e
gerijeb <gtpepi@hotmail.com>
2021-12-27 22:00:26 -0300

    Add loop3_2, a loop exercise from JavaScript.info

---
+++ math/inputtest.js
@@ -0,0 +1,5 @@
+let userInput; 
+
+do {
+    userInput = prompt ('Are you a bot? Please type a number greater than 100', 0)
+} while (userInput <= 100 && userInput);
---
+++ math/loop3_2.js
@@ -0,0 +1,9 @@
+let nn = 10; 
+
+nextPrime: 
+for (let i = 2; i <=nn; i++) { // 2
+    for (let j = 2; j < i; j++) { // 2
+        if (i % j === 0) continue nextPrime;
+    }    
+    console.log(i); 
+}
