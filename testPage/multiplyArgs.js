ba535d31a83bdebc32d0fa3eb7f5f3fb37b4d295
gerijeb <gtpepi@hotmail.com>
2022-01-04 23:52:36 -0300

    Add sorter.js, rearragen HTML

--- testPage/multiplyArgs.js
+++ testPage/multiplyArgs.js
@@ -3,17 +3,17 @@ const multiplyBtn = document.querySelector("#multiplyArgsBtn");
 const multiplyResult = document.querySelector("#multiplyArgsResult");
 
 function multiplyArgs(multiplier, ...theArgs) {
-    console.log("Received args with rest function: " + multiplier, theArgs);
+    //console.log("Received args with rest function: " + multiplier, theArgs);
     return theArgs.map(element => {
         return multiplier*element;
     })
 }
 
 multiplyBtn.addEventListener('click', () => {
-    console.log("Raw value from input: " + multiplyInpt.value + ' ' + typeof(multiplyInpt.value));
+    //console.log("Raw value from input: " + multiplyInpt.value + ' ' + typeof(multiplyInpt.value));
     const input = multiplyInpt.value.split(",")
-    console.log("Value after spliting and turning into array: " + input + ' ' + typeof(input));
+    //console.log("Value after spliting and turning into array: " + input + ' ' + typeof(input));
     const neatInput = input.map(Number);
-    console.log("Without rest function: " + neatInput + ' ' + typeof(neatInput));
+    //console.log("Without rest function: " + neatInput + ' ' + typeof(neatInput));
     multiplyResult.textContent = multiplyArgs(...neatInput);
 });
---
+++ testPage/sorter.js
@@ -0,0 +1,21 @@
+const sortBtn = document.querySelector("#sort1");
+const sortInpt = document.querySelector("#sort1Inpt");
+const sortResult = document.querySelector("#sort1Result");
+
+sortRestArgs = (...theArgs) => {
+    let sortedArgs = theArgs.sort();
+    return sortedArgs;
+};
+
+sortBtn.addEventListener("click", () => {
+    const newInput = sortInpt.value.split(",");
+    const neatInput = newInput.map(Number);
+    sortResult.textContent = sortRestArgs(...neatInput);
+});
+
+/* restArgs can be sorted because they have properties like real Arrays. 
+The EventListener does a trick by transforming the input from str to array and 
+passing the value as an array though.*/
+
+
+
