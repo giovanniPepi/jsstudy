0ed02f8a970cf284587d52086ecfccb6d91a4ebb
gerijeb <gtpepi@hotmail.com>
2022-01-12 15:03:25 -0300

    Add final results. Add reload option

--- rockpaperscissor/game.js
+++ rockpaperscissor/game.js
@@ -3,8 +3,11 @@ let justPlayed = null;
 let usrScore = 0;
 let pcScore = 0; 
 const resultArea = document.querySelector(".resultArea")
-const resultHistoryArea = document.querySelector(".resultHistoryArea");
 
+function addReloadBtn () {
+    const reloadOption = document.querySelector(".reload");
+    reloadOption.addEventListener('click', () => window.location.reload(false));
+}
 function pcPlay() {
     const turn = ['rock', 'paper', 'scissors'];
     const pcRand = Math.floor(Math.random() * turn.length);
@@ -15,36 +18,42 @@ function gamePlay (usrTurn, pcTurn) {
     if ((usrTurn != 'rock') && (usrTurn != 'paper') && (usrTurn != 'scissors')) {
             return(`Please type 'Rock', 'Paper' or 'Scissors' to play.`);
         } else if (usrTurn  == pcTurn) {
-            writeRound (usrTurn, pcTurn);
             ++usrScore;
             ++pcScore;
+            writeRound (usrTurn, pcTurn);
             return(`It\'s a Draw! You choose ${usrTurn}, computer chooses ${pcTurn} too.`); 
         } else if (pcTurn == 'scissors' && usrTurn  == 'paper') {
-            writeRound (usrTurn, pcTurn);
             ++pcScore;
+            writeRound (usrTurn, pcTurn);
             return(`Nooo! You have lost, you choose ${usrTurn}, computer chooses ${pcTurn}.`);
         } else if (pcTurn == 'paper' && usrTurn  == 'rock') {
-            writeRound (usrTurn, pcTurn);
             ++pcScore;
+            writeRound (usrTurn, pcTurn);
             return(`Noo! You have lost, you choose ${usrTurn}, computer chooses ${pcTurn}.`);
         } else if (pcTurn == 'rock' && usrTurn  == 'scissors') {
-            writeRound (usrTurn, pcTurn);
             ++pcScore;
+            writeRound (usrTurn, pcTurn);
             return(`Nooo! You have lost, you choose ${usrTurn}, computer chooses ${pcTurn}.`);
         } else {
-            writeRound (usrTurn, pcTurn);
             ++usrScore;
+            writeRound (usrTurn, pcTurn);
             return(`YEAH! You have won, you choose ${usrTurn}, computer chooses ${pcTurn}.`)
         }
     };
 
-function playThis (n) {
+function playThis (n) {    
     const keyChoice = document.querySelector(`.keyChoice[data-key="${n.keyCode}"]`);
     if (keyChoice == null) return; // not a game key
     if (n.keyCode === justPlayed) return; // avoids CSS stuck when repeated input
     keyChoice.classList.add('playing');
     justPlayed = n.keyCode;
     createResultDiv((gamePlay(keyChoice.id, pcPlay())));     
+
+    if (usrScore >= 5 || pcScore >=5) {
+        getFinalResults();
+        window.removeEventListener('keydown', playThis);
+        addReloadBtn();       
+    }
 };
 
 function removeTransition (n) {
@@ -61,36 +70,24 @@ function createResultDiv (result) {
         resultArea.appendChild(resultDiv);        
     } else {
         resultDiv.textContent = result;
-        archiveResult();
     };
-};
-
-function archiveResult () {
-    const last = document.querySelector('#history');
-    last.textContent = "Last results:";
-    const historyDiv = document.createElement("div");
-    const currentResult = document.querySelector('.results').textContent;
-
-    historyDiv.setAttribute("class", "resultHistory");
-    historyDiv.textContent = currentResult;
-    resultHistoryArea.appendChild(historyDiv);    
 };
 
 function writeRound () {
     const header = document.querySelector(".header");
     const userResult = document.querySelector(".userPlay");
     const pcResult = document.querySelector(".pcPlay");
+    const pWarning = document.querySelector(".warning");
 
+    if (pWarning) header.removeChild(pWarning);    
     if (!userResult) {
         const userResult = document.createElement("div");    
         userResult.setAttribute("class", "userPlay");
         userResult.textContent = 'User score: ' + usrScore;
         header.appendChild(userResult);
-
     } else {
         userResult.textContent = 'User score: ' + usrScore;
     };
-
     if (!pcResult) {
         const pcResult = document.createElement("div");
         pcResult.setAttribute("class", "pcPlay");
@@ -101,6 +98,40 @@ function writeRound () {
         pcResult.textContent = 'Computer score: ' + pcScore;
     }    
 };
+
+function removeScoreChild () {
+    const header = document.querySelector('.header');
+    const results = document.querySelector('.resultArea');
+
+    while (header.firstChild) {
+        header.removeChild(header.firstChild);
+    };
+    while(results.firstChild) {
+        results.removeChild(results.firstChild);
+    }
+}
+
+function getFinalResults () { 
+    const header = document.querySelector(".header");
+    removeScoreChild();
+    const finalPlacar = document.createElement("div");
+    finalPlacar.setAttribute("class", "finalPlacar");
+    
+    const reloadOption = document.createElement("button")
+    reloadOption.setAttribute("class", "reload");
+    reloadOption.textContent="Play Again!";
+
+    if (usrScore > pcScore) {
+        finalPlacar.textContent = "Final Score. You: " + usrScore + ". Computer: " +pcScore + ". " + "Congratulations!";
+    } else if (usrScore < pcScore) {
+        finalPlacar.textContent = "Final Score. You: " + usrScore + ". Computer: " +pcScore + ". " + "Pathetic.";
+    } else if (usrScore === pcScore) {
+        finalPlacar.textContent = "Final Score. You: " + usrScore + ". Computer: " +pcScore + ". " + "It was a tough one!";
+    }
+    header.appendChild(finalPlacar);
+    header.appendChild(reloadOption);   
+};
+
 
 const choices = document.querySelectorAll('.keyChoice');
 choices.forEach(choice => choice.addEventListener('transitionend', removeTransition));
