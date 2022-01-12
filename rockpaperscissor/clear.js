ef108761d6e4135771975d72305d7639717377a6
gerijeb <gtpepi@hotmail.com>
2022-01-12 16:18:48 -0300

    Merge UI to main, remove img dir, remove clear.js

--- rockpaperscissor/clear.js
+++
@@ -1,9 +0,0 @@
-const clearBtn = document.querySelector("#clear");
-
-clearBtn.addEventListener("click", () => {
-    const historyDiv = document.querySelectorAll(".resultHistory");
-    historyDiv.forEach(key => resultHistoryArea.removeChild(key)); 
-    
-    const last = document.querySelector('#history');
-    last.textContent = "";
-})
