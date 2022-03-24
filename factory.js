50ff150f91959a25f1e87debd7f8a6fe1d6e19c9
gtpepi <gtpepi@hotmail.com>
2022-03-23 21:52:40 -0300

    Add 'console.log({})' explanation.

--- factory.js
+++ factory.js
@@ -1,9 +1,20 @@
 const personFactory = (name, age) => {
     const sayHello = () => console.log ('Hello!'); 
+    /* This was updated in ES6 */ 
     return {name, age, sayHello};
-};
+    };
 
 const jeff = personFactory ('jeff', 27);
 console.log(jeff.name);
 jeff.sayHello();
 console.log(jeff.age);
+
+/* The console log ({}) trick */
+
+const personName = "Maynard"; 
+const color = 'red';
+const number = 34; 
+const food = "rice";
+
+console.log (personName, color, number, food);
+console.log({personName, color, number, food});
