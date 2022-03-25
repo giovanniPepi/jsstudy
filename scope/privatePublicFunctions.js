cac35577da46ed1c94530d207f9c3ea60db2efe6
gtpepi <gtpepi@hotmail.com>
2022-03-25 17:14:05 -0300

    Update private functions examples

--- scope/privatePublicFunctions.js
+++ scope/privatePublicFunctions.js
@@ -6,12 +6,29 @@ const FactoryFunction = string => {
 
 const taco = FactoryFunction('taco');
 
-printString(); // ERROR!!
-capitalizeString(); // ERROR!!
-taco.capitalizeString(); // ERROR!!
+//printString(); // ERROR!!
+//capitalizeString(); // ERROR!!
+//taco.capitalizeString(); // ERROR!!
 
 // only the returned function will work
 // this is closure: functions retain their scope 
 // even if they are passed around and called outside the scope
 
 taco.printString();
+
+const counterCreator = () => {
+    let count = 0;
+    return () => {
+        console.log(count); 
+        count++;
+    };
+};
+
+// to use the returned function, we assign it to a variable
+const counter = counterCreator();
+counter();
+counter();
+counter();
+counter();
+counter();
+counter();
